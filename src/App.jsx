f3d35fd77701aff4c8b6ce606b1d281bb75c2f75
haqueashraful <123ashrafulhaque@gmail.com>
2024-03-15 23:08:43 +0600

    enviornment setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Component/Banner";
+import Nav from "./Component/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Banner />
+    </>
+  );
+}
+
+export default App;
